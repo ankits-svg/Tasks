c2e091ae10d0d2659dec3720f32418f495271ee6
Ankit Sharma <ankits745@gmail.com>
2023-12-07 13:43:19 +0530

    further updates

--- metric_converter/client/src/Components/Footer.jsx
+++ metric_converter/client/src/Components/Footer.jsx
@@ -14,7 +14,6 @@ const Footer = () => {
         { unit: 'ft-us', measurement: 'Length' },
         { unit: 'ft', measurement: 'Length' },
         { unit: 'mi', measurement: 'Length' },
-      
         { unit: 'mm2', measurement: 'Area' },
         { unit: 'cm2', measurement: 'Area' },
         { unit: 'm2', measurement: 'Area' },
@@ -24,7 +23,6 @@ const Footer = () => {
         { unit: 'ft2', measurement: 'Area' },
         { unit: 'ac', measurement: 'Area' },
         { unit: 'mi2', measurement: 'Area' },
-      
         { unit: 'mcg', measurement: 'Mass' },
         { unit: 'mg', measurement: 'Mass' },
         { unit: 'g', measurement: 'Mass' },
@@ -33,7 +31,6 @@ const Footer = () => {
         { unit: 'lb', measurement: 'Mass' },
         { unit: 'mt', measurement: 'Mass' },
         { unit: 't', measurement: 'Mass' },
-      
         { unit: 'mm3', measurement: 'Volume' },
         { unit: 'cm3', measurement: 'Volume' },
         { unit: 'ml', measurement: 'Volume' },
@@ -51,16 +48,12 @@ const Footer = () => {
         { unit: 'gal', measurement: 'Volume' },
         { unit: 'ft3', measurement: 'Volume' },
         { unit: 'yd3', measurement: 'Volume' },
-      
         { unit: 'mm3/s', measurement: 'Volume Flow Rate' },
         { unit: 'cm3/s', measurement: 'Volume Flow Rate' },
-        // ... continue adding rows for other Volume Flow Rate units
-      
         { unit: 'C', measurement: 'Temperature' },
         { unit: 'F', measurement: 'Temperature' },
         { unit: 'K', measurement: 'Temperature' },
         { unit: 'R', measurement: 'Temperature' },
-      
         { unit: 'ns', measurement: 'Time' },
         { unit: 'mu', measurement: 'Time' },
         { unit: 'ms', measurement: 'Time' },
@@ -71,22 +64,16 @@ const Footer = () => {
         { unit: 'week', measurement: 'Time' },
         { unit: 'month', measurement: 'Time' },
         { unit: 'year', measurement: 'Time' },
-      
         { unit: 'Hz', measurement: 'Frequency' },
         { unit: 'mHz', measurement: 'Frequency' },
-        // ... continue adding rows for other Frequency units
-      
         { unit: 'm/s', measurement: 'Speed' },
         { unit: 'km/h', measurement: 'Speed' },
         { unit: 'm/h', measurement: 'Speed' },
         { unit: 'knot', measurement: 'Speed' },
         { unit: 'ft/s', measurement: 'Speed' },
-      
         { unit: 's/m', measurement: 'Pace' },
         { unit: 'min/km', measurement: 'Pace' },
         { unit: 's/ft', measurement: 'Pace' },
-        // ... continue adding rows for other Pace units
-      
         { unit: 'Pa', measurement: 'Pressure' },
         { unit: 'hPa', measurement: 'Pressure' },
         { unit: 'kPa', measurement: 'Pressure' },
@@ -95,7 +82,6 @@ const Footer = () => {
         { unit: 'torr', measurement: 'Pressure' },
         { unit: 'psi', measurement: 'Pressure' },
         { unit: 'ksi', measurement: 'Pressure' },
-      
         { unit: 'b', measurement: 'Digital' },
         { unit: 'Kb', measurement: 'Digital' },
         { unit: 'Mb', measurement: 'Digital' },
@@ -106,41 +92,33 @@ const Footer = () => {
         { unit: 'MB', measurement: 'Digital' },
         { unit: 'GB', measurement: 'Digital' },
         { unit: 'TB', measurement: 'Digital' },
-      
         { unit: 'lx', measurement: 'Illuminance' },
         { unit: 'ft-cd', measurement: 'Illuminance' },
-      
         { unit: 'ppm', measurement: 'Parts-Per' },
         { unit: 'ppb', measurement: 'Parts-Per' },
         { unit: 'ppt', measurement: 'Parts-Per' },
         { unit: 'ppq', measurement: 'Parts-Per' },
-      
         { unit: 'V', measurement: 'Voltage' },
         { unit: 'mV', measurement: 'Voltage' },
         { unit: 'kV', measurement: 'Voltage' },
-      
         { unit: 'A', measurement: 'Current' },
         { unit: 'mA', measurement: 'Current' },
         { unit: 'kA', measurement: 'Current' },
-      
         { unit: 'W', measurement: 'Power' },
         { unit: 'mW', measurement: 'Power' },
         { unit: 'kW', measurement: 'Power' },
         { unit: 'MW', measurement: 'Power' },
         { unit: 'GW', measurement: 'Power' },
-      
         { unit: 'VA', measurement: 'Apparent Power' },
         { unit: 'mVA', measurement: 'Apparent Power' },
         { unit: 'kVA', measurement: 'Apparent Power' },
         { unit: 'MVA', measurement: 'Apparent Power' },
         { unit: 'GVA', measurement: 'Apparent Power' },
-      
         { unit: 'VAR', measurement: 'Reactive Power' },
         { unit: 'mVAR', measurement: 'Reactive Power' },
         { unit: 'kVAR', measurement: 'Reactive Power' },
         { unit: 'MVAR', measurement: 'Reactive Power' },
         { unit: 'GVAR', measurement: 'Reactive Power' },
-      
         { unit: 'Wh', measurement: 'Energy' },
         { unit: 'mWh', measurement: 'Energy' },
         { unit: 'kWh', measurement: 'Energy' },
@@ -148,13 +126,11 @@ const Footer = () => {
         { unit: 'GWh', measurement: 'Energy' },
         { unit: 'J', measurement: 'Energy' },
         { unit: 'kJ', measurement: 'Energy' },
-      
         { unit: 'VARh', measurement: 'Reactive Energy' },
         { unit: 'mVARh', measurement: 'Reactive Energy' },
         { unit: 'kVARh', measurement: 'Reactive Energy' },
         { unit: 'MVARh', measurement: 'Reactive Energy' },
         { unit: 'GVARh', measurement: 'Reactive Energy' },
-      
         { unit: 'deg', measurement: 'Angle' },
         { unit: 'rad', measurement: 'Angle' },
         { unit: 'grad', measurement: 'Angle' },
